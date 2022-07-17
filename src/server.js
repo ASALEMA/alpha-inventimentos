46b9e41dd0bf9d9fa4bf1b6b73f30c9dcfc290ea
Aline Salema <alineelainesalema@gmail.com>
2022-07-17 14:33:59 -0300

    Criação da estrutura inicial do projeto.

---
+++ src/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const app = require('./api');
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (_request, response) => {
+    response.send();
+});
+
+app.listen(port, () => console.log('ouvindo na porta', port));
