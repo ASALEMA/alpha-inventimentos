252a0a6e7d4e38f8af855b0334fe235be0fe4ad3
Aline Salema <alineelainesalema@gmail.com>
2022-07-17 15:02:37 -0300

    Adição do eslint e nodemon.

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = require('./api');
 const port = process.env.PORT || 3000;
 
 app.get('/', (_request, response) => {
-    response.send();
+  response.send();
 });
 
 app.listen(port, () => console.log('ouvindo na porta', port));
