3549f71e3cfa5e6dfd0dd5c57cea223df33f22d4
Aline Salema <alineelainesalema@gmail.com>
2022-07-21 21:24:55 -0300

    Implementação de compra da corretora

--- src/models/clienteModel.js
+++ src/models/clienteModel.js
@@ -7,6 +7,51 @@ const comprar = async (codCliente, codAtivo, qtdeAtivo) => {
   return result.insertId;
 };
 
+const atualizarCarteira = async (codCliente, codAtivo, qtdeAtivo) => {
+  const query = 'UPDATE alpha_investimentos_api_db.carteira_cliente  SET qtd_ativo = ? WHERE  cod_cliente = ? AND  cod_ativo = ?;';
+  await connection.execute(query, [qtdeAtivo, codCliente, codAtivo]);
+};
+
+const salvarHistoricoOperacao = async (
+  codCliente,
+  codAtivo,
+  codTipoOperacao,
+  qtdAtivo,
+  valorAtivo,
+) => {
+  const query = 'INSERT INTO alpha_investimentos_api_db.cliente_historico_operacao (cod_cliente, cod_ativo, cod_tipo_operacao, qtd_ativo, val_ativo) VALUES (?,?,?,?,?);';
+  const [result] = await connection
+    .execute(query, [codCliente, codAtivo, codTipoOperacao, qtdAtivo, valorAtivo]);
+
+  return result.insertId;
+};
+
+const obterAtivoCarteiraCliente = async (codCliente, codAtivo) => {
+  const query = 'SELECT * FROM alpha_investimentos_api_db.carteira_cliente WHERE cod_cliente = ? AND cod_ativo = ?';
+  const [result] = await connection.execute(query, [codCliente, codAtivo]);
+
+  return !result || result.length === 0
+    ? null
+    : result[0];
+};
+
+const obterCarteiraCliente = async (codCliente) => {
+  const query = `
+    SELECT * 
+    FROM alpha_investimentos_api_db.carteira_cliente AS carteira
+    INNER JOIN alpha_investimentos_api_db.ativo AS ativo ON ativo.cod_ativo = carteira.cod_ativo
+    WHERE cod_cliente = ?;`;
+  const [result] = await connection.execute(query, [codCliente]);
+
+  return !result || result.length === 0
+    ? null
+    : result;
+};
+
 module.exports = {
   comprar,
+  salvarHistoricoOperacao,
+  obterAtivoCarteiraCliente,
+  atualizarCarteira,
+  obterCarteiraCliente,
 };
--- src/models/corretoraModel.js
+++ src/models/corretoraModel.js
@@ -9,6 +9,36 @@ const obterQuantidadeDisponivel = async (codAtivo) => {
     : result[0].qtd_ativo_disponivel;
 };
 
+const obterAtivosDisponiveis = async (codAtivo) => {
+  const query = `
+    SELECT carteira.cod_ativo, carteira.qtd_ativo_disponivel, ativo.val_ativo
+    FROM alpha_investimentos_api_db.carteira_corretora AS carteira
+    INNER JOIN alpha_investimentos_api_db.ativo AS ativo ON ativo.cod_ativo = carteira.cod_ativo
+    WHERE carteira.cod_ativo = ?;`;
+  const [result] = await connection.execute(query, [codAtivo]);
+
+  return !result || result.length === 0
+    ? null
+    : result;
+};
+
+const atualizarCarteira = async (codAtivo, qtdeAtivo) => {
+  const query = 'UPDATE alpha_investimentos_api_db.carteira_corretora SET qtd_ativo_disponivel = ? WHERE cod_ativo = ?;';
+  await connection.execute(query, [qtdeAtivo, codAtivo]);
+};
+
+const obterValorAtivo = async (codAtivo) => {
+  const query = 'SELECT * FROM alpha_investimentos_api_db.ativo WHERE  cod_ativo = ?';
+  const [result] = await connection.execute(query, [codAtivo]);
+
+  return !result || result.length === 0
+    ? 0
+    : result[0].val_ativo;
+};
+
 module.exports = {
   obterQuantidadeDisponivel,
+  obterValorAtivo,
+  obterAtivosDisponiveis,
+  atualizarCarteira,
 };
